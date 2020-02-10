e45022f344d802dd5d2d3a11f3ad8ebce2a35b99
Hansen Liu <hansen.liu@mail.mcgill.ca>
2020-02-10 15:57:34 -0500

    Added unit test framework

--- image_repo/config.py
+++ image_repo/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    # SECRET_KEY = '78b81502f7e89045fe634e85d02f42c5'
+    SECRET_KEY = '78b81502f7e89045fe634e85d02f42c5'
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     # MAIL_SERVER = 'smtp.gmail.com'
     # MAIL_PORT = 587
@@ -9,7 +9,7 @@ class Config:
     # MAIL_USERNAME = 'flaskblog12345@gmail.com111'
     # MAIL_PASSWORD = 'testerpw1'
 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///images.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
